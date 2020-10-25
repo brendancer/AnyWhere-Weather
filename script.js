8b558fd37dfe99056773bff14806f150efe73f86
Brenda Goodwin <brendanced@gmail.com>
2020-10-24 20:05:44 -0600

    City Data

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
 var apikey = "5a9330ff39aa4d71dda59e8146ab11fd";
-var cityName;
-console.log(cityName);
-
-var queryURL =
-  "https://api.openweathermap.org/data/2.5/weather?q=London&appid=" + apikey;
-
-console.log(queryURL);
-
+var lon;
+var lat;
 $("#saveCity").click(function () {
   var cityName = $("#cityName").val();
-  console.log("extracting the input value from: ", $("#cityName"));
-});
-$.ajax({
-  url: queryURL,
-  method: "GET",
-}).then(function (response) {
-  console.log(response);
+  console.log(cityName);
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&appid=" +
+    apikey +
+    "&units=imperial";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+
+    //find longitude and latitude of city
+    var long = response.coord.lon;
+    var lat = response.coord.lat;
+    console.log(long);
+
+    var query1URL =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      lat +
+      "&lon=" +
+      long +
+      "&appid=" +
+      apikey +
+      "&units=imperial";
+
+    $.ajax({
+      url: query1URL,
+      method: "GET",
+    }).then(function (response) {
+      //display city dat
+      console.log(response);
+
+      var temp = response.current.temp;
+      $("#temp").html("Temperature: " + temp);
+
+      var humid = response.current.humidity;
+      $("#humidity").html("Humidity: " + humid);
+
+      var wind = response.current.wind_speed;
+      $("#wind-speed").html("Wind Speed: " + wind);
+
+      var uvi = response.current.uvi;
+      $("#UV").html("UV index: : " + uvi);
+    });
+  });
 });
